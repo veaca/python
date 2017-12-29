7e7a97ae9b2d396808b5f360dcd0f43bc1788f30
veaca <32932112+veaca@users.noreply.github.com>
2017-12-30 00:39:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,8 +56,6 @@ def handle_message(event):
         elif jenis.lower() == 'group':
             group_id = simpan['source']['groupId']
             line_bot_api.leave_group(group_id)
-    elif txtpesan.lower() == 'index' :
-	line_bot_api.reply_message(reply_token, TextSendMessage(text = soup.get_text()))
     else :
         line_bot_api.reply_message(reply_token, TextSendMessage(text = txtpesan))
     
