ce2a34d6cacb6885cb54ddb26edd7587b67c489b
veaca <32932112+veaca@users.noreply.github.com>
2017-12-30 23:13:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,9 +50,9 @@ def handle_message(event):
     if txtpesan.lower() == 'coba' :
         url = 'https://en.wikipedia.org/wiki/Indonesia'
         page = requests.get(url)
-        page_soup = soup(page.text, 'html.parser')
-        tampil = str(page_soup)
-        line_bot_api.reply_message(reply_token, TextSendMessage(text = tampil))
+        #page_soup = soup(page.text, 'html.parser')
+        #tampil = str(page_soup)
+        line_bot_api.reply_message(reply_token, TextSendMessage(text = page.status_code))
     elif txtpesan.lower() == 'leave' :
         jenis = simpan['source']['type']
         if jenis.lower() == 'room':
